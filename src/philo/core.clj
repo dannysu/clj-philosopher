4de8e3b6afb2ab04cd656e98a2b298038895c431
Danny Su <contact@dannysu.com>
2012-10-04 04:05:55 -0400

    Proper wait & block when fork is taken
    Allowing for finer grain dosync

--- src/philo/core.clj
+++ src/philo/core.clj
@@ -1,31 +1,102 @@
-(ns philo.core)
+(ns philo.core
+  (:use [clojure.tools.cli :only [cli]]
+        [clojure.data.json :only [json-str]])
+  (:gen-class))
+
+(defn now [] (System/currentTimeMillis))
+
+(defn make-free-fork
+  []
+  (ref (deliver (promise) :delivered)))
+
+(defn make-taken-fork
+  []
+  (ref (promise)))
 
 (defn fork-indices
+  "Determine fork indices for a philosopher"
   [philo-id num-philo]
   [philo-id (mod (dec philo-id) num-philo)])
 
+(def deref-x2 (comp deref deref))
+(defn deref-x2-timeout
+  [timeout]
+  (fn [x] (deref (deref x) timeout nil)))
+
+(def not-nil? (comp not nil?))
+
+(def free? (comp realized? deref))
+(def taken? (comp not free?))
+
 (defn grab-forks
-  [philo-id table]
+  "Thread-safe function to grab 2 forks needed by a philosopher"
+  [philo-id table & [timeout]]
   (let [indices (fork-indices philo-id (count table))
-        forks (map #(nth table %) indices)]
-    (loop []
-      (if (= [nil nil] (map deref forks))
+        forks (map #(nth table %) indices)
+        deref-fn (if (nil? timeout) deref-x2 (deref-x2-timeout timeout))]
+
+    ; deref and potentially wait for all required forks to become available
+    (let [fork1 (deref-fn (first forks))
+          fork2 (deref-fn (second forks))]
+      ; Claim the forks by swapping in new promises to put them back.
+      ; It's a race for everyone! Whoever commits the transaction wins.
+      ; Everyone else retries and gets blocked until promises are delivered.
+      (when (and (not-nil? fork1) (not-nil? fork2))
         (doseq [f forks]
-          (ref-set f philo-id))
-        ;(println "oh well")
-        ;(throw (Exception. "Couldn't get forks"))
-        (let [p (promise)
-              watch-fn (fn [k r o n] (deliver p nil))]
-          (add-watch (first forks) nil watch-fn)
-          (@p)
-          (recur))))
+          (ref-set f (promise)))
+        ))
+
+    ; return resulting state of the world
     table))
 
 (defn drop-forks
   [philo-id table]
   (let [indices (fork-indices philo-id (count table))
         forks (map #(nth table %) indices)]
-    (when (= [philo-id philo-id] (map deref forks))
+    (when (= [true true] (map taken? forks))
       (doseq [f forks]
-        (ref-set f nil)))
+        (deliver (deref f) :delivered)))
     table))
+
+(defn action [action philo-id log duration]
+	(send-off log conj [(now) philo-id action duration])
+	(Thread/sleep duration))
+
+(def eat (partial action :eating))
+(def think (partial action :thinking))
+
+(defn philosopher [philo-id numtimes log table]
+  (dotimes [_ numtimes]
+    (think philo-id log (rand-int 200))
+    (dosync 
+      (grab-forks philo-id table))
+    (eat philo-id log (rand-int 200))
+    (dosync
+      (drop-forks philo-id table))))
+
+(defn dinner [philocount numtimes log]
+	(let [table (repeatedly philocount make-free-fork)]
+		(doall 
+			(pmap (fn [philo-id] 
+				(philosopher philo-id numtimes log table)) 
+				(range philocount)))))
+
+(defn run 
+  "Run the simulation and print the output"
+  [philocount times]
+	(let [log (agent [])]
+		  (println (str "Table of " philocount " philosophers, each eating " times " times" ))
+		  (dinner philocount times log)
+		  (Thread/sleep 1000)
+		  (println (json-str @log)))
+	(shutdown-agents))
+
+(defn -main [& args]
+	(let [[opts args banner]
+		 	(cli args
+			     ["-s" "--philocount" "number of philosophers/forks" :parse-fn #(Integer. %)] 
+			     ["-n" "--times" "number of iterations each philosopher eats" :parse-fn #(Integer. %)])]
+		(if
+	        (and (:philocount opts) (:times opts))
+      		(run (:philocount opts) (:times opts))
+      		(println banner))))
--- test/philo/core_test.clj
+++ test/philo/core_test.clj
@@ -2,26 +2,68 @@
   (:use clojure.test
         philo.core))
 
+(deftest make-free-fork-test
+         (is (= true
+                (realized? (deref (make-free-fork))))))
+
+(deftest make-taken-fork-test
+         (is (= false
+                (realized? (deref (make-taken-fork))))))
+
 (deftest fork-indices-test
          (is (= [0 4] (fork-indices 0 5)))
          (is (= [1 0] (fork-indices 1 5))))
 
 (deftest grab-forks-test
-         (is (= [0 nil nil nil 0]
+         (is (= [false true true true false]
                 (dosync
-                  (map deref (grab-forks 0 (map ref [nil nil nil nil nil]))))))
-         (is (= [1 1 nil nil nil]
-                (dosync
-                  (map deref (grab-forks 0 (map ref [1 1 nil nil nil]))))))
-         )
+                  (map free?
+                       (grab-forks 0 (repeatedly 5 make-free-fork))))))
+         (is (= [false false true true true]
+                (let [busy-table (concat (repeatedly 2 make-taken-fork) (repeatedly 3 make-free-fork))]
+                  (dosync
+                    (map free?
+                         ; provide a timeout for test purposes because grab-forks is expected to block
+                         (grab-forks 0 busy-table 100)))))))
 
 (deftest drop-forks-test
-         (is (= [nil nil nil nil nil]
-                (dosync
-                  (map deref (drop-forks 0 (map ref [0 nil nil nil 0])))))))
+         (is (= [true true true true true]
+                (let [busy-table (concat (repeatedly 2 make-taken-fork) (repeatedly 3 make-free-fork))]
+                  (dosync
+                    (map free?
+                         (drop-forks 1 busy-table))))))
+         (is (= [true true true false false]
+                (let [busy-table (concat (repeatedly 3 make-free-fork) (repeatedly 2 make-taken-fork))]
+                  (dosync
+                    (map free?
+                         (drop-forks 1 busy-table)))))))
 
-;(deftest eat-test
-         ;(is (= 0 1)))
+(deftest eat-logging 
+	(is 
+		(= [2 :eating 200]
+			(let [log (agent [])]
+				(eat 2 log 200)
+				(rest (first @log))))))
 
-;(deftest think-test
-         ;(is (= 0 1)))
+(deftest think-test
+	(is 
+		(= [2 :thinking 150]
+			(let [log (agent [])]
+				(think 2 log 150)
+				(rest (first @log))))))
+
+(deftest philosopher-test
+	(is
+		(= 20
+			(let [log (agent [])]
+				(philosopher 1 10 log (repeatedly 5 make-free-fork))
+				(Thread/sleep 1000)
+				(count @log)))))
+
+(deftest dinner-test
+	(is
+		(= 50
+			(let [log (agent [])]
+				(dinner 5 5 log)
+				(Thread/sleep 1000)
+				(count @log)))))
